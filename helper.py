ecf54618c4dc30caf2623f2fbd136cebb03a4a96
guoliang.he <guoliang.he@s-cube.com>
2020-12-15 16:21:54 +0000

    add

--- helper.py
+++ helper.py
@@ -5,3 +5,25 @@ import torch.nn as nn
 """
 helper class for training
 """
+
+class helper:
+
+    @staticmethod
+    def batcher(image):
+        pass
+
+
+
+class dnn_utils:
+
+    @staticmethod
+    def train(model, data):
+        return 
+
+    
+    @staticmethod
+    def test(model, data):
+        return 
+
+
+        
